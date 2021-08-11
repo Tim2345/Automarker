65bf94f18558320696ea635a8ad3283c72b53740
Tim2345 <timmyelks@icloud.com>
2021-08-11 21:08:51 +0100

    vanilla dataset class for multitask learning

---
+++ MTLearning/multitask_dataset.py
@@ -0,0 +1,17 @@
+import torch
+
+class MultitaskDataset(torch.utils.data.Dataset):
+    '''
+    Bare bones data loader for multitask datasets
+    '''
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
